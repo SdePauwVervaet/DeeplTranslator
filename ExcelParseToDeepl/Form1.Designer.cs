80b5c0d2e495c12eaa5ad6eae6803ceea31b58cf
Sander de Pauw <Sander.dePauw@vervaet.nl>
2023-11-20 16:34:07 +0100

    Changed Designer Component Names

--- ExcelParseToDeepl/Form1.Designer.cs
+++ ExcelParseToDeepl/Form1.Designer.cs
@@ -29,128 +29,127 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.Open = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            TextBox_GlossaryFiles = new TextBox();
+            Button_GenerateGlossaries = new Button();
+            Button_GlossarySelection = new Button();
+            Button_TranslationFolderSelection = new Button();
+            TextBox_TranslationFiles = new TextBox();
+            Button_TranslateAlerts = new Button();
+            textBox3 = new TextBox();
+            Button_TranslateLanguages = new Button();
+            SuspendLayout();
             // 
-            // textBox1
+            // TextBox_GlossaryFiles
             // 
-            this.textBox1.AllowDrop = true;
-            this.textBox1.Location = new System.Drawing.Point(13, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(362, 23);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
-            this.textBox1.DragOver += new System.Windows.Forms.DragEventHandler(this.textBox1_DragOver);
+            TextBox_GlossaryFiles.AllowDrop = true;
+            TextBox_GlossaryFiles.Location = new Point(13, 12);
+            TextBox_GlossaryFiles.Name = "TextBox_GlossaryFiles";
+            TextBox_GlossaryFiles.Size = new Size(362, 23);
+            TextBox_GlossaryFiles.TabIndex = 1;
+            TextBox_GlossaryFiles.TextChanged += TextBoxGlossarySelect_OnTextChanged;
+            TextBox_GlossaryFiles.DragDrop += TextBoxGlossarySelect_DragDrop;
+            TextBox_GlossaryFiles.DragOver += TextBoxGlossarySelect_DragOver;
             // 
-            // button1
+            // Button_GenerateGlossaries
             // 
-            this.button1.Location = new System.Drawing.Point(539, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(199, 24);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Generate glossary files";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            Button_GenerateGlossaries.Location = new Point(539, 13);
+            Button_GenerateGlossaries.Name = "Button_GenerateGlossaries";
+            Button_GenerateGlossaries.Size = new Size(199, 24);
+            Button_GenerateGlossaries.TabIndex = 2;
+            Button_GenerateGlossaries.Text = "Generate glossary files";
+            Button_GenerateGlossaries.UseVisualStyleBackColor = true;
+            Button_GenerateGlossaries.Click += ButtonGenerateGlossaries_OnClick;
             // 
-            // Open
+            // Button_GlossarySelection
             // 
-            this.Open.Location = new System.Drawing.Point(381, 12);
-            this.Open.Name = "Open";
-            this.Open.Size = new System.Drawing.Size(152, 24);
-            this.Open.TabIndex = 3;
-            this.Open.Text = "Select Glossary Excel...";
-            this.Open.UseVisualStyleBackColor = true;
-            this.Open.Click += new System.EventHandler(this.Open_Click);
+            Button_GlossarySelection.Location = new Point(381, 12);
+            Button_GlossarySelection.Name = "Button_GlossarySelection";
+            Button_GlossarySelection.Size = new Size(152, 24);
+            Button_GlossarySelection.TabIndex = 3;
+            Button_GlossarySelection.Text = "Select Glossary Excel...";
+            Button_GlossarySelection.UseVisualStyleBackColor = true;
+            Button_GlossarySelection.Click += ButtonSelectGlossaryFiles_OnClick;
             // 
-            // button2
+            // Button_TranslationFolderSelection
             // 
-            this.button2.Location = new System.Drawing.Point(381, 74);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(152, 24);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Select folder to translate...";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.Open_Click_2);
+            Button_TranslationFolderSelection.Location = new Point(381, 74);
+            Button_TranslationFolderSelection.Name = "Button_TranslationFolderSelection";
+            Button_TranslationFolderSelection.Size = new Size(152, 24);
+            Button_TranslationFolderSelection.TabIndex = 4;
+            Button_TranslationFolderSelection.Text = "Select folder to translate...";
+            Button_TranslationFolderSelection.UseVisualStyleBackColor = true;
+            Button_TranslationFolderSelection.Click += ButtonSelectTranslationFiles_OnClick;
             // 
-            // textBox2
+            // TextBox_TranslationFiles
             // 
-            this.textBox2.AllowDrop = true;
-            this.textBox2.Location = new System.Drawing.Point(13, 74);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(362, 23);
-            this.textBox2.TabIndex = 5;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            TextBox_TranslationFiles.AllowDrop = true;
+            TextBox_TranslationFiles.Location = new Point(13, 74);
+            TextBox_TranslationFiles.Name = "TextBox_TranslationFiles";
+            TextBox_TranslationFiles.Size = new Size(362, 23);
+            TextBox_TranslationFiles.TabIndex = 5;
+            TextBox_TranslationFiles.TextChanged += TextBoxTranslationFolderSelect_OnTextChanged;
             // 
-            // button3
+            // Button_TranslateAlerts
             // 
-            this.button3.Location = new System.Drawing.Point(539, 60);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(199, 24);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "Translate \'Alert\' files in folder";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            Button_TranslateAlerts.Location = new Point(539, 60);
+            Button_TranslateAlerts.Name = "Button_TranslateAlerts";
+            Button_TranslateAlerts.Size = new Size(199, 24);
+            Button_TranslateAlerts.TabIndex = 6;
+            Button_TranslateAlerts.Text = "Translate 'Alert' files in folder";
+            Button_TranslateAlerts.UseVisualStyleBackColor = true;
+            Button_TranslateAlerts.Click += ButtonTranslateAlerts_OnClick;
             // 
             // textBox3
             // 
-            this.textBox3.AllowDrop = true;
-            this.textBox3.Location = new System.Drawing.Point(13, 153);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
-            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox3.Size = new System.Drawing.Size(725, 348);
-            this.textBox3.TabIndex = 7;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            textBox3.AllowDrop = true;
+            textBox3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox3.Location = new Point(13, 153);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.ScrollBars = ScrollBars.Vertical;
+            textBox3.Size = new Size(725, 348);
+            textBox3.TabIndex = 7;
             // 
-            // button4
+            // Button_TranslateLanguages
             // 
-            this.button4.Location = new System.Drawing.Point(539, 90);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(200, 23);
-            this.button4.TabIndex = 8;
-            this.button4.Text = "Translate Language files in folder";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            Button_TranslateLanguages.Location = new Point(539, 90);
+            Button_TranslateLanguages.Name = "Button_TranslateLanguages";
+            Button_TranslateLanguages.Size = new Size(200, 23);
+            Button_TranslateLanguages.TabIndex = 8;
+            Button_TranslateLanguages.Text = "Translate Language files in folder";
+            Button_TranslateLanguages.UseVisualStyleBackColor = true;
+            Button_TranslateLanguages.Click += ButtonTranslateLanguages_OnClick;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(747, 509);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.Open);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
-            this.Text = "Deepl Glossaries from Excel & Update translation files";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(747, 509);
+            Controls.Add(Button_TranslateLanguages);
+            Controls.Add(textBox3);
+            Controls.Add(Button_TranslateAlerts);
+            Controls.Add(TextBox_TranslationFiles);
+            Controls.Add(Button_TranslationFolderSelection);
+            Controls.Add(Button_GlossarySelection);
+            Controls.Add(Button_GenerateGlossaries);
+            Controls.Add(TextBox_GlossaryFiles);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Deepl Glossaries from Excel & Update translation files";
+            Load += MainForm_OnLoad;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-        private TextBox textBox1;
-        private Button button1;
-        private Button Open;
-        private Button button2;
-        private TextBox textBox2;
-        private Button button3;
+        private TextBox TextBox_GlossaryFiles;
+        private Button Button_GenerateGlossaries;
+        private Button Button_GlossarySelection;
+        private Button Button_TranslationFolderSelection;
+        private TextBox TextBox_TranslationFiles;
         private TextBox textBox3;
-        private Button button4;
+        private Button Button_TranslateAlerts;
+        private Button Button_TranslateLanguages;
     }
 }
--- ExcelParseToDeepl/Form1.cs
+++ ExcelParseToDeepl/Form1.cs
@@ -8,32 +8,55 @@ namespace DeeplTranslator
             Logger.InitializeLogger(textBox3);
         }
 
-        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        private void TextBoxGlossarySelect_OnTextChanged(object sender, EventArgs e)
         {
-            if (e.Data.GetData(DataFormats.FileDrop) is string[] files && files.Any())
-                textBox1.Text = files.First(); //select the first one  
+
         }
 
-        private void textBox1_DragOver(object sender, DragEventArgs e)
+        private void TextBoxGlossarySelect_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetData(DataFormats.FileDrop) is string[] files && files.Any())
+                TextBox_GlossaryFiles.Text = files.First(); //select the first one  
+        }
+
+        private void TextBoxGlossarySelect_DragOver(object sender, DragEventArgs e)
         {
             e.Effect = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Link : DragDropEffects.None;
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void TextBoxTranslationFolderSelect_OnTextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
+
+        private void ButtonSelectTranslationFiles_OnClick(object sender, EventArgs e)
         {
-            if (textBox1.Text.Length == 0)
+            using var dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+
+            TextBox_TranslationFiles.AppendText(dialog.SelectedPath);
+        }
+
+        private void ButtonSelectGlossaryFiles_OnClick(object sender, EventArgs e)
+        {
+            using var dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                textBox1.Text = @"Select a file by dropping it here...";
-                textBox1.Update();
+                TextBox_GlossaryFiles.Text = dialog.FileName;
+            }
+        }
+        
+        private void ButtonGenerateGlossaries_OnClick(object sender, EventArgs e)
+        {
+            if (TextBox_GlossaryFiles.Text.Length == 0)
+            {
+                TextBox_GlossaryFiles.Text = @"Select a file by dropping it here...";
+                TextBox_GlossaryFiles.Update();
             }
             else
             {
-                var file = new FileInfo(textBox1.Text);
+                var file = new FileInfo(TextBox_GlossaryFiles.Text);
                 string extn = file.Extension;
                 System.Diagnostics.Debug.WriteLine("File Extension: {0}", extn);
 
@@ -46,57 +69,15 @@ namespace DeeplTranslator
                 }
                 else
                 {
-                    textBox1.Text = @"I'll only 'eat' .xlsx files. Select a file by dropping it here...";
-                    textBox1.Update();
+                    TextBox_GlossaryFiles.Text = @"I'll only 'eat' .xlsx files. Select a file by dropping it here...";
+                    TextBox_GlossaryFiles.Update();
                 }
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private async void ButtonTranslateAlerts_OnClick(object sender, EventArgs e)
         {
-
-        }
-
-        private void Open_Click(object sender, EventArgs e)
-        {
-            using var dialog = new OpenFileDialog();
-            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                textBox1.Text = dialog.FileName;
-            }
-        }
-        private void Open_Click_2(object sender, EventArgs e)
-        {
-            using var dialog = new FolderBrowserDialog();
-            DialogResult result = dialog.ShowDialog();
-
-            textBox2.AppendText(dialog.SelectedPath);
-        }
-
-        private void textBox2_DragDrop(object sender, DragEventArgs e)
-        {
-            if (e.Data.GetData(DataFormats.FileDrop) is string[] files && files.Any())
-                textBox1.Text = files.First(); //select the first one  
-        }
-
-        private void textBox2_DragOver(object sender, DragEventArgs e)
-        {
-            e.Effect = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Link : DragDropEffects.None;
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private async void button3_Click(object sender, EventArgs e)
-        {
-            var files = Directory.GetFiles(textBox2.Text, "*.*", SearchOption.AllDirectories)
+            var files = Directory.GetFiles(TextBox_TranslationFiles.Text, "*.*", SearchOption.AllDirectories)
                     .Where(file => new[] { ".json" }
                     .Contains(Path.GetExtension(file)))
                     .ToList();
@@ -106,14 +87,9 @@ namespace DeeplTranslator
             await translatorService.TranslateAlertFiles(files);
         }
 
-        private void textBox3_TextChanged(object sender, EventArgs e)
+        private async void ButtonTranslateLanguages_OnClick(object sender, EventArgs e)
         {
-
-        }
-
-        private async void button4_Click(object sender, EventArgs e)
-        {
-            var files = Directory.GetFiles(textBox2.Text, "*.*", SearchOption.AllDirectories)
+            var files = Directory.GetFiles(TextBox_TranslationFiles.Text, "*.*", SearchOption.AllDirectories)
                     .Where(file => new string[] { ".js" }
                     .Contains(Path.GetExtension(file)))
                     .ToList();
@@ -126,6 +102,16 @@ namespace DeeplTranslator
 
             var translatorService = new TranslatorService();
             await translatorService.TranslateLanguageFiles(files);
+        }
+
+        private void MainForm_OnLoad(object sender, EventArgs e)
+        {
+            System.Drawing.Rectangle workingRectangle = Screen.PrimaryScreen.WorkingArea;
+
+            //Sets window size to half the primary-screens resolution.
+            this.Size = new System.Drawing.Size(Convert.ToInt32(0.5 * workingRectangle.Width), Convert.ToInt32(0.5 * workingRectangle.Height));
+
+            this.Location = new System.Drawing.Point(10, 10);
         }
     }
 }
